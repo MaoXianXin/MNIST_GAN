5b0e7261ec73ac229ee0aedf98d61bef3532f289
MaoXianxin <qq1044467857@gmail.com>
2019-02-20 18:11:50 +0800

    save checkpoint

--- MNIST_GAN.py
+++ MNIST_GAN.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import datetime
+import os
 import matplotlib.pyplot as plt
 # %matplotlib inline
 
@@ -96,6 +97,7 @@ z_dimensions = 100
 #     plt.imshow(generated_image, cmap='Greys')
 #
 tf.reset_default_graph()
+sess = tf.Session()
 batch_size = 50
 
 z_placeholder = tf.placeholder(tf.float32, [None, z_dimensions], name='z_placeholder')
@@ -137,27 +139,30 @@ tf.summary.scalar('Generator_loss', g_loss)
 tf.summary.scalar('Discriminator_loss_real', d_loss_real)
 tf.summary.scalar('Discriminator_loss_fake', d_loss_fake)
 
-images_for_tensorboard = generator(z_placeholder, batch_size, z_dimensions)
-tf.summary.image('Generated_images', images_for_tensorboard, 5)
+# images_for_tensorboard = generator(z_placeholder, batch_size, z_dimensions)
+# tf.summary.image('Generated_images', images_for_tensorboard, 5)
 merged = tf.summary.merge_all()
 logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
 writer = tf.summary.FileWriter(logdir, sess.graph)
 
-sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
+# Add ops to save and restore all the variables.
+saver = tf.train.Saver()
+logdir = "./checkpoint/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/model.ckpt"
+
 # Pre-train discriminator
-for i in range(300):
+for i in range(1000):
     z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
     real_image_batch = mnist.train.next_batch(batch_size)[0].reshape([batch_size, 28, 28, 1])
     _, __, dLossReal, dLossFake = sess.run([d_trainer_real, d_trainer_fake, d_loss_real, d_loss_fake],
                                            {x_placeholder: real_image_batch, z_placeholder: z_batch})
 
-    if(i % 100 == 0):
-        print("dLossReal:", dLossReal, "dLossFake:", dLossFake)
+    # if(i % 100 == 0):
+    #     print("dLossReal:", dLossReal, "dLossFake:", dLossFake)
 
 # Train generator and discriminator together
-for i in range(100000):
+for i in range(1000000):
     real_image_batch = mnist.train.next_batch(batch_size)[0].reshape([batch_size, 28, 28, 1])
     z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
 
@@ -165,17 +170,20 @@ for i in range(100000):
     _, __, dLossReal, dLossFake = sess.run([d_trainer_real, d_trainer_fake, d_loss_real, d_loss_fake],
                                            {x_placeholder: real_image_batch, z_placeholder: z_batch})
 
-    # Train generator
+    # Train generator twice
     z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
     _ = sess.run(g_trainer, feed_dict={z_placeholder: z_batch})
 
-    if i % 10 == 0:
+    z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
+    _ = sess.run(g_trainer, feed_dict={z_placeholder: z_batch})
+
+    if i % 100 == 0:
         # Update TensorBoard with summary statistics
         z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
         summary = sess.run(merged, {z_placeholder: z_batch, x_placeholder: real_image_batch})
         writer.add_summary(summary, i)
 
-    if i % 100 == 0:
+    if i % 5000 == 0:
         # Every 100 iterations, show a generated image
         print("Iteration:", i, "at", datetime.datetime.now())
         z_batch = np.random.normal(0, 1, size=[1, z_dimensions])
@@ -184,8 +192,8 @@ for i in range(100000):
         plt.imshow(images[0].reshape([28, 28]), cmap='Greys')
         plt.show()
 
-        # Show discriminator's estimate
-        im = images[0].reshape([1, 28, 28, 1])
-        result = discriminator(x_placeholder)
-        estimate = sess.run(result, {x_placeholder: im})
-        print("Estimate:", estimate)
+    if i % 200000 == 0:
+        # Save the variables to disk.
+        os.makedirs(logdir)
+        save_path = saver.save(sess, logdir)
+        print("Model saved in path: %s" % save_path)
